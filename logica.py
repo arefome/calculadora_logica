e7697d8f737a219869eb8da0ccfe015f2adf397b
JoseDRamirezM <eskatejose@hotmail.com>
2020-05-13 10:49:28 -0500

    operaciones

---
+++ logica.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+
+def op_or(var1, var2):
+	return np.logica_or(var1, var2)
+
+def op_and(var1, var2):
+	return np.logical_and(var1,var2)
+
+def op_condicional(var1, var2):	
+	return (negar(var1) | var2)
+
+def op_bicondicional(var1, var2):
+	return negar(np.logical_xor(var1,var2))
+
+def negar(var):
+	return np.logical_not(var)
+
