bc7fdd4ba6a54faf84c87e470730e0a7822e5af4
arefome <55114188+arefome@users.noreply.github.com>
2020-05-13 19:07:12 -0500

    Update VentanaCalculadora.py

--- VentanaCalculadora.py
+++ VentanaCalculadora.py
@@ -87,8 +87,8 @@ class VentanaCalculadora(tk.Tk):
     
     def mostrar_resultado(self):
         try:    
-            aux = cadena.proposicion(self.input.get())
-            var = cadena.variables(self.input.get())
+            aux = cadena.proposicion(self.entrada.get())
+            var = cadena.variables(self.entrada.get())
             table = ttg.Truths(var, [aux])
             print(aux)
             print(table)
